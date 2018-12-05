896e91a97771d4e3aa6e425ca9993bd9335f20a4
eguido52 <eguido@csumb.edu>
2018-12-05 12:21:57 -0800

    Updated Db

--- src/main/java/edu/csumb/cst438/chipsdb/ChipsdbApplication.java
+++ src/main/java/edu/csumb/cst438/chipsdb/ChipsdbApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ChipsdbApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ChipsdbApplication.class, args);
 	}
